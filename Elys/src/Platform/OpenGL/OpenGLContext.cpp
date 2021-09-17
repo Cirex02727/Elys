2839345a9397f0ed3e8476f99101e50c94762f83
cirex02727 <samy2002cirex@gmail.com>
2021-09-17 15:41:00 +0200

    First Triangle

--- Elys/src/Platform/OpenGL/OpenGLContext.cpp
+++ Elys/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Elys {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ELYS_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		ELYS_CORE_INFO("OpenGL Info:");
+		ELYS_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		ELYS_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		ELYS_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
